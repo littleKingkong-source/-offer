91f1ae7679da8faa9199852a6fbc4e05d850b2a9
littleKingkong-source <3472202368@qq.com>
2021-03-25 09:17:17 +0800

    Offer009,用两个栈实现队列

---
+++ Offer09.java
@@ -0,0 +1,92 @@
+package referoffer;
+import java.util.Scanner;
+/**
+ * @author crazy邹
+ * @create 2021/3/24 15:30
+ */
+
+/**
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。(若队列中没有元素，deleteHead 操作返回 -1 )
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ * @author crazy邹
+ */
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ *["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ *
+ */
+//方法一官方解法
+/*class CQueue {
+    Deque<Integer> stack1;
+    Deque<Integer> stack2;
+    public CQueue() {
+        stack1 = new LinkedList<Integer>();
+        stack2 = new LinkedList<Integer>();
+    }
+
+    public void appendTail(int value) {
+        stack1.push(value);
+    }
+
+    public int deleteHead() {
+        if (stack2.isEmpty()){
+            while (!stack1.isEmpty()){
+                stack2.push((stack1.pop()));
+            }
+        }
+        if (stack2.isEmpty()){
+            return -1;
+        }else{
+            int deleteItem = stack2.pop();
+            return deleteItem;
+        }
+    }
+}*/
+//方法二
+class CQueue{
+    LinkedList<Integer> A,B;
+    public CQueue(){
+        A = new LinkedList<Integer>();
+        B = new LinkedList<Integer>();
+    }
+    public void appendTail(int value) {
+        A.addLast(value);
+
+    }
+    public int deleteHead() {
+        if(!B.isEmpty()){ return B.removeLast();}
+        if (A.isEmpty()) {return -1;}
+        while (!A.isEmpty()){
+            B.addLast(A.removeLast());
+        }
+        return B.removeLast();
+    }
+}
+public class Offer09 {
+    public static void main(String[] args) {
+        CQueue queue = new CQueue();
+        System.out.println("您想输入几个数呢");
+        int num = new Scanner(System.in).nextInt();
+        System.out.print("请连续输入数据：");
+        int val;
+        for (int i = 1;i <= num;i++ ){
+            val = new Scanner(System.in).nextInt();
+            queue.appendTail(val);
+        }
+        for (int i = 1;i <= num;i++){
+            System.out.print(queue.deleteHead() + " ");
+        }
+    }
+}
