88f7d8d230ece578d98e89ade66c6ab29ded20ea
littleKingkong-source <71617127+littleKingkong-source@users.noreply.github.com>
2021-03-25 09:37:49 +0800

    Update Offer09.java
    添加了注释

--- Offer09.java
+++ Offer09.java
@@ -29,63 +29,70 @@ import java.util.LinkedList;
  */
 //方法一官方解法
 /*class CQueue {
+    //声明两个栈stack1，stack2；
     Deque<Integer> stack1;
     Deque<Integer> stack2;
     public CQueue() {
+    //分配存储空间；
         stack1 = new LinkedList<Integer>();
         stack2 = new LinkedList<Integer>();
     }
-
+    //入栈操作；
     public void appendTail(int value) {
         stack1.push(value);
     }
-
+    //出栈；
     public int deleteHead() {
+    //判断栈stack2是否为空；
         if (stack2.isEmpty()){
-            while (!stack1.isEmpty()){
+        //如果为空，则可以把栈stack1的元素入stack2栈；
+            while (!stack1.isEmpty()){//stack1不为空，把元素出栈入stack2；
                 stack2.push((stack1.pop()));
             }
         }
-        if (stack2.isEmpty()){
+        if (stack2.isEmpty()){//如果stack2也没有元素了，则返回-1；
             return -1;
-        }else{
+        }else{//否则返回stack2出栈的元素，则对应着队列“先进先出”的顺序出队列；
             int deleteItem = stack2.pop();
             return deleteItem;
         }
     }
 }*/
 //方法二
-class CQueue{
-    LinkedList<Integer> A,B;
-    public CQueue(){
+class CQueue{//声明双向列表A,B；
+    LinkedList<Integer> A,B;//LinkedList是双向列表；
+    public CQueue(){//分配相应的存储空间，泛型<Interger>.
+        //A为出栈，的列表LinkedList可以用来当栈使用；
+        //B为入栈，的列表；
         A = new LinkedList<Integer>();
         B = new LinkedList<Integer>();
     }
-    public void appendTail(int value) {
+    public void appendTail(int value) {//添加元素；
         A.addLast(value);
 
     }
     public int deleteHead() {
+        //判断B是否为空，B不为空返回列表最后一个元素，相当于出栈对应后进先出的栈；
         if(!B.isEmpty()){ return B.removeLast();}
-        if (A.isEmpty()) {return -1;}
-        while (!A.isEmpty()){
+        if (A.isEmpty()) {return -1;}//如果用来出栈的B栈也空了，则返回题目要求的-1；
+        while (!A.isEmpty()){//如果用来入栈的，栈A，不为空，则向栈B添加，A出栈的元素，从而完成倒叙，使得栈B的出栈就可以对应队列的出队顺序了；
             B.addLast(A.removeLast());
         }
-        return B.removeLast();
+        return B.removeLast();//返回用来作为出栈的栈B当中的元素；
     }
 }
 public class Offer09 {
     public static void main(String[] args) {
-        CQueue queue = new CQueue();
+        CQueue queue = new CQueue();//创建对象queue
         System.out.println("您想输入几个数呢");
         int num = new Scanner(System.in).nextInt();
         System.out.print("请连续输入数据：");
         int val;
-        for (int i = 1;i <= num;i++ ){
+        for (int i = 1;i <= num;i++ ){//向栈A中输入元素；
             val = new Scanner(System.in).nextInt();
             queue.appendTail(val);
         }
-        for (int i = 1;i <= num;i++){
+        for (int i = 1;i <= num;i++){//输出栈B中的元素，也就是对应着队列当中的出队顺序；
             System.out.print(queue.deleteHead() + " ");
         }
     }
